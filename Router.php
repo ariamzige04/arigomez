beef412a18ef0337038420dd2efea2d5b1a1912c
ariamzige04 <74326414+ariamzige04@users.noreply.github.com>
2022-04-04 19:24:03 -0500

    Add files via upload

---
+++ Router.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace MVC;
+
+class Router
+{
+    public array $getRoutes = [];
+    public array $postRoutes = [];
+
+    public function get($url, $fn) {
+        $this->getRoutes[$url] = $fn;
+    }
+
+    public function post($url, $fn) {
+        $this->postRoutes[$url] = $fn;
+    }
+
+    public function comprobarRutas() {
+
+        session_start();
+        $auth = $_SESSION['login'] ?? null;
+
+        //Arreglo de rutas protegidas
+        $rutas_protegidas = ['/admin' ];
+        
+        //Url actual
+        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if ($method === 'GET') {
+            $fn = $this->getRoutes[$currentUrl] ?? null;
+        } else {
+            $fn = $this->postRoutes[$currentUrl] ?? null;
+        }
+
+        //Proteger las rutas
+        //$currentUrl es $urlActual ()
+        if(in_array($currentUrl, $rutas_protegidas) && !$auth) {
+            header('Location: /');
+        }
+
+        if ( $fn ) {
+            // Call user fn va a llamar una función cuando no sabemos cual sera
+            call_user_func($fn, $this); // This es para pasar argumentos
+        } else {
+            header('Location: /pagina_no_encontrada');
+        }
+    }
+
+    public function render($view, $datos = []) {
+        // Leer lo que le pasamos  a la vista
+        foreach ($datos as $key => $value) {
+            $$key = $value;  // Doble signo de dolar significa: variable variable, básicamente nuestra variable sigue siendo la original, pero al asignarla a otra no la reescribe, mantiene su valor, de esta forma el nombre de la variable se asigna dinamicamente
+        }
+
+        ob_start(); // Almacenamiento en memoria durante un momento...
+
+        // entonces incluimos la vista en el layout
+        include_once __DIR__ . "/views/$view.php";
+        $contenido = ob_get_clean(); // Limpia el Buffer
+        include_once __DIR__ . '/views/layout.php';
+    }
+}
